184f6a215205df287644b5def08a7f2c58e316c8
butlersl <157722385+butlersl@users.noreply.github.com>
2026-04-20 20:30:20 -0400

    Delete Keylogger/uninstaller.cmd

--- Keylogger/uninstaller.cmd
+++
@@ -1,8 +0,0 @@
-cd C:/Users/%USERNAME%/AppData/Roaming/Microsoft/Windows && cd "Start Menu" && cd Programs/Startup
-del c.cmd
-cd %TEMP%
-del l.ps1 
-del p.ps1
-del DdBPKCytRe
-del %USERNAME%.log
-taskkill /im powershell.exe /f
